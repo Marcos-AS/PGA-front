5f6d28adbe8a354902a615435a65e449ab93ec41
Marcos-AS <marcossantangelo1@gmail.com>
2025-04-07 18:22:22 -0300

    agregué text area y fetch para interactuar con la ia

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,12 +9,28 @@ export default {
   },
   data() {
     return {
-      cursoMsg: "curso"
+      prompt: "",
+      resp: ""
     };
   },
   methods: {
     async comprarCurso() {
       // tu lógica aquí
+    },
+    async enviar() {
+      console.log("Método enviar() activado con prompt:", this.prompt);
+
+      const res = await fetch("http://localhost:8080/api/llama", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ prompt: this.prompt })
+      })
+      console.log(res);
+      
+      const text = await res.text();
+      this.resp = text;
     }
   }
 }
@@ -24,6 +40,13 @@ export default {
   <div class="greetings">
     <h1>Plataforma de Generación de Aprendizaje Dinámico</h1>
     <h3>
+      <div>
+        <h1>Chat con Llama.cpp</h1>
+        <textarea v-model="prompt" placeholder="Escribí algo..."></textarea>
+        <button @click="enviar">Enviar</button>
+        <p><strong>Respuesta:</strong> {{ resp }}</p>
+      </div>
+      
       <PaypalButton />
       <PaymentForm />
     </h3>
@@ -45,6 +68,11 @@ h3 {
 .greetings h1,
 .greetings h3 {
   text-align: center;
+}
+
+textarea {
+  width: 100%;
+  height: 80px;
 }
 
 @media (min-width: 1024px) {
